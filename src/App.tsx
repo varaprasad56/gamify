29001e2cf55d8b5f8e603ad411447ac53673e46e
MVaraPrasad <varaprasad.512@gmail.com>
2024-12-10 22:10:45 +0530

    Added Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Button, HStack, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
-  return <div>Gamify test</div>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <HStack>
+      <Button onClick={() => setCounter(counter - 1)}>Decrement</Button>
+      <Text>{counter}</Text>
+      <Button onClick={() => setCounter(counter + 1)}>Increment</Button>
+    </HStack>
+  );
 }
 
 export default App;
